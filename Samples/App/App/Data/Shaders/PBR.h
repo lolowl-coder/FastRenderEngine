c5c88c1a521b0d6ada1be227c4feddf484865f3a
sv <ivanov161@gmail.com>
2025-11-18 12:24:22 -0800

    Comments, prepare for cuda-toolkit update to v13.0

--- Samples/App/App/Data/Shaders/PBR.h
+++ Samples/App/App/Data/Shaders/PBR.h
@@ -3,18 +3,9 @@
 
 #include "Constants.h"
 
-// PBR shading formula used in this project:
+// Rendering equation used in this project:
 // Lo(x, V) = Le(x, V) + SumOverSamplesCount(Fr(x, L, V) * Li(x, L) * NdotL)
-// Where:
-// Lo(x, V) - Outgoing radiance (reflected light) at point x toward view direction V
-// Le(x, V) - Emissive color
-// Fr(x, L, V) - Bidirectional Reflectance Distribution Function (BRDF)
-// Li(x, L) - Incoming light radiance. For directional light this is just the light radiance.
-// (NdotL) - Simple angular term known from Blin-Phong lighting model
-// x - surface point
-// V - view direction (from surface point toward camera)
-// L - light direction (from surface point toward light)
-// N - surface normal at point x
+// Below is the breakdown of it.
 
 // Math helpers
 float saturate(float x)
@@ -69,7 +60,22 @@ struct PBRInputs
     float roughness;
 };
 
-// PBR shading function
+//PBR shading function.
+// Assuming rendering equation is:
+// Lo(x, V) = Le(x, V) + SumOverSamplesCount(Fr(x, L, V) * Li(x, L) * NdotL)
+// Where:
+// Lo(x, V) - Outgoing radiance (reflected light) at point x toward view direction V
+// Le(x, V) - Emissive color
+// Fr(x, L, V) - Bidirectional Reflectance Distribution Function (BRDF)
+// Li(x, L) - Incoming light radiance. For directional light this is just the light radiance.
+// (NdotL) - Simple angular term known from Blin-Phong lighting model
+// x - surface point
+// V - view direction (from surface point toward camera)
+// L - light direction (from surface point toward light)
+// N - surface normal at point x
+// this function calculates following part of rendering equation:
+// SumOverSamplesCount(Fr(x, L, V) * Li(x, L) * NdotL)
+// Emissive term Le(x, V) should be added separately.
 vec3 shadePBR(const PBRInputs I)
 {
 	// Halfway vector
@@ -113,13 +119,13 @@ vec3 shadePBR(const PBRInputs I)
 	//NdotL is applied later
     vec3 diff = kd * I.baseColor / M_PI;
     
-    //Lo(x, V) = Le(x, V) + SumOverSamplesCount(Fr(x, L, V) * Li(x, L) * NdotL)
+    //SumOverSamplesCount(Fr(x, L, V) * Li(x, L) * NdotL)
     return
         // Fr(x, L, V)
         (diff + spec) *
         // Li(x, L)
         I.radiance *
-		// Angular term
+		// Angular term NdotL
         NdotL;
 }
 
